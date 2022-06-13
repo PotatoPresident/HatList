80c4656179f2f25fc4569fd3fe9558802139ff4a
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2022-06-12 19:27:32 -0700

    Update to 1.19

--- src/main/java/us/potatoboy/hatlist/client/HatListClient.java
+++ src/main/java/us/potatoboy/hatlist/client/HatListClient.java
@@ -1,14 +1,8 @@
 package us.potatoboy.hatlist.client;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.mojang.authlib.GameProfile;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-
-import java.util.UUID;
 
 @Environment(EnvType.CLIENT)
 public class HatListClient implements ClientModInitializer {
